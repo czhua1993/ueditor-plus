3a850c1c8444947cfd8f1e6b1648ca26bae241bb
zeno.chen <zeno.chen@ubtrobot.com>
2022-11-25 16:06:00 +0800

    fix(wordimage): 转存没有选择对应文件时，提示并可以确认

--- dialogs/wordimage/wordimage.js
+++ dialogs/wordimage/wordimage.js
@@ -36,6 +36,9 @@ function addUploadButtonListener() {
           if ($('#fileUrl').val().indexOf(file.name) >= 0) {
             $('#fileUrl').val(res.url);
             $('.image-tip').html('<span style="color:#139213;">转存成功，请提交确认</span>');
+          } else {
+            $('#fileUrl').val(res.url);
+            $('.image-tip').html('<span style="color:#139213;">上传成功，请提交确认</span>');
           }
         } else {
           $('.image-tip').html('上传失败:' + JSON.stringify(res))
@@ -64,14 +67,14 @@ function addOkListener() {
       if (!src) continue;
       for (var j = 0, url; url = imageUrls[j++];) {
         // console.log('url',src, url);
-        if (src.indexOf(url.name.replace(" ", "")) != -1) {
+        // if (src.indexOf(url.name.replace(" ", "")) != -1) {
           img.src = urlPrefix + url.url;
           img.setAttribute("_src", urlPrefix + url.url);  //同时修改"_src"属性
           img.setAttribute("title", url.title);
           domUtils.removeAttributes(img, ["data-word-image", "style", "width", "height"]);
           editor.fireEvent("selectionchange");
           break;
-        }
+        // }
       }
     }
     editor.fireEvent('saveScene');
